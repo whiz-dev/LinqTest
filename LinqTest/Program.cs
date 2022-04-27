0eea7dfacb4c61e2a0a9c9a09d01e40183aa9cc2
whiz-dev <whiz123@hanmail.net>
2022-04-27 20:23:32 +0900

    초기 커밋.

---
+++ LinqTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열 sort
+            int[] nums = new int[] { 1, 3, 2, 5, 7, 2 };
+            nums = nums.OrderBy(num => num).ToArray();
+            nums = nums.OrderByDescending(num => num).ToArray();
+
+            //배열 filter
+            string[] chars = new string[] { "aaa", "bbb", "aac" };
+            chars = chars.Where(c => c.Contains("a")).ToArray();
+
+
+
+
+            //Sum, Average, Min, Max
+            double avg = nums.Average();
+            avg = nums.Average(num => num * 2);
+
+            //Distinct, Reverse, OrderBy, OrderByDescending
+
+            //Where, Skip, SkipWhile, Take, TakeWhile, First, Last, Single
+
+            //Any, All, Contains
+
+            //Aggregate
+
+            //Union, Except Intersect, Concate
+
+            //Select, GroupBy
+
+
+            Console.ReadLine();
+        }
+    }
+}
