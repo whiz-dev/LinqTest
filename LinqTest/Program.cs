7eeb54fdf5cfe629d18bb2291d118ba493dc5e3e
whiz-dev <whiz123@hanmail.net>
2022-06-13 21:32:32 +0900

    Join 추가

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -11,8 +11,14 @@ namespace LinqTest
         public class Student
         {
             public string Name { get; set; }
-            public string Class { get; set; }
+            public string ClassId { get; set; }
             public int Score { get; set; }
+        }
+
+        public class Class
+        {
+            public string ClassId { get; set; }
+            public string ClassName { get; set; }
         }
 
         static void Main(string[] args)
@@ -22,17 +28,27 @@ namespace LinqTest
             string[] chars = new string[] { "aaa", "bbb", "aac" };
             List<Student> students = new List<Student>
             {
-                new Student { Name = "aaa", Class="A", Score = 88 },
-                new Student { Name = "bbb", Class="A", Score = 50 },
-                new Student { Name = "ccc", Class="A", Score = 70 },
-                new Student { Name = "ddd", Class="B", Score = 45 },
-                new Student { Name = "eee", Class="B", Score = 84 },
-                new Student { Name = "fff", Class="B", Score = 98 }
+                new Student { Name = "aaa", ClassId="A", Score = 88 },
+                new Student { Name = "bbb", ClassId="A", Score = 50 },
+                new Student { Name = "ccc", ClassId="A", Score = 70 },
+                new Student { Name = "ddd", ClassId="B", Score = 45 },
+                new Student { Name = "eee", ClassId="B", Score = 84 },
+                new Student { Name = "fff", ClassId="B", Score = 98 }
+            };
+            List<Class> classes = new List<Class>
+            {
+                new Class { ClassId = "A", ClassName="Class A" },
+                //new Class { ClassId = "B", ClassName="Class B" }
             };
 
 
             /**************************************************************
-             * Sum, Average, Min, Max, Count, Aggregate
+             * Sum : 요소들의 전체 합계
+             * Average : 요소들의 평균
+             * Min : 요소들의 최소값
+             * Max : 요소들의 최대값
+             * Count : 요소들의 갯수
+             * Aggregate : 요소들의 누적 계산 값
              *************************************************************/
             int numsSum = nums.Sum();
             double numsAvg = nums.Average();
@@ -43,13 +59,20 @@ namespace LinqTest
             string charsAggregate = chars.Aggregate((char1, char2) => char1 + "," + char2);
 
             /**************************************************************
-             * Distinct, Reverse
+             * Distinct : 중복 제거한 결과
+             * Reverse : 역배열 결과
              *************************************************************/
             int[] numsDistinct = nums.Distinct().ToArray();
             int[] numsReverse = nums.Reverse().ToArray();
 
             /**************************************************************
-             * Skip, SkipWhile, Take, TakeWhile, First, Last, Single
+             * Skip : 인덱스 만큼 건너뛴 나머지 배열
+             * SkipWhile : 해당 조건을 만족하지 않는 요소가 나올때까지 건너뜀
+             * Take : 인덱스 만큼 포함한 배열
+             * TakeWhile : 해당 조건을 만족하지 않는 요소가 나올때까지 포함
+             * First : 배열의 첫번째 요소
+             * Last : 배열의 마지막 요소
+             * Single : 지정한 조건을 만족하는 요소를 반환. 조건을 만족하는 요소가 2개 이상이면 예외 발생
              *************************************************************/
             int[] numsSkip = nums.Skip(3).ToArray();
             int[] numsSkipWhile = nums.SkipWhile(num => num == 5).ToArray();
@@ -60,14 +83,19 @@ namespace LinqTest
             try { nums.Single(num => num >= 5); } catch { Console.WriteLine("Not Single!"); }
 
             /**************************************************************
-             * Any, All, Contains
+             * Any : 지정한 조건을 만족하는 요소가 1개라도 있는지 여부
+             * All : 지정한 조건을 모든 요소가 만족하는지 여부
+             * Contains : 해당 요소를 포함하는지 여부
              *************************************************************/
             bool numsAny = nums.Any(num => num == 7);
             bool numsAll = nums.All(num => num == 7);
             bool numsContains = nums.Contains(7);
 
             /**************************************************************
-             * Union, Except Intersect, Concate
+             * Union : 합집합
+             * Except : 차집합
+             * Intersect : 교집합
+             * Concate : 두 배열 이어 붙이기 (Union All)
              *************************************************************/
             int[] numsUnion = nums.Union(nums1).ToArray();
             int[] numsExcept = nums.Except(nums1).ToArray();
@@ -75,14 +103,32 @@ namespace LinqTest
             int[] numsConcate = nums.Concat(nums1).ToArray();
 
             /**************************************************************
-             * Select, Where, GroupBy, OrderBy, OrderByDescending
+             * Select : 새로운 요소로 변환하여 반환
+             * Where : 지정한 조건을 만족하는 요소만 반환
+             * GroupBy : 지정한 조건으로 Grouping
+             * OrderBy : 오름차순 정렬
+             * OrderByDescending : 내림차순 정렬
              *************************************************************/
             string[] numsSelect = nums.Select(num => num.ToString().PadLeft(5, '0')).ToArray();
             int[] numsWhere = nums.Where(num => num > 3).ToArray();
-            var lstClassAverage = students.GroupBy(student => student.Class).Select(group => new { Class = group.Key, Average = group.Average(student => student.Score) }).ToList();
+            var lstClassAverage = students.GroupBy(student => student.ClassId).Select(group => new { Class = group.Key, Average = group.Average(student => student.Score) }).ToList();
             int[] numsOrderBy = nums.OrderBy(num => num).ToArray();
             int[] numsOrderByDesc = nums.OrderByDescending(num => num).ToArray();
-            
+
+            /**************************************************************
+             * Join : 새로운 요소로 변환하여 반환
+             * GroupJoin : 지정한 조건을 만족하는 요소만 반환
+             *************************************************************/
+            var lstStudentClassJoin = students.Join(classes,
+                                                    student => new { ClassId = student.ClassId },
+                                                    cls => new { ClassId = cls.ClassId },
+                                                    (person, cls) => new { Name = person.Name, Class = cls.ClassName, Score = person.Score });
+
+            var lstStudentClassGroupJoin = students.GroupJoin(classes,
+                                                              student => student.ClassId,
+                                                              cls => cls.ClassId,
+                                                              (person, clsGroup) => new { Name = person.Name, Class = clsGroup.Count() > 0 ? clsGroup.First().ClassName : null, Score = person.Score });
+
 
             Console.ReadLine();
         }
